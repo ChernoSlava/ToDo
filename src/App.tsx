5c7e385e040d072a257513945d2f8e3c6d39738c
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-02-18 15:40:26 +0300

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,16 @@
-import React, { useReducer } from "react";
-import { ToDoContext } from "@contexts";
-
+import React from "react";
+import { Provider } from 'react-redux';
 import styles from './App.css';
-
-import { AddInputContainer, ToDoListContainer} from '@containers';
-
-import { ToDoListReducer, InitialTodoList } from './store';
-
-// const ToDoListReducer = (state, action) => {
-//     const {type, payload} = action;
-
-//     switch (type) {
-//         //dispatch({type: 'EDIT', payload: { id: 1, title: 'buy cat food' }})
-//         case 'EDIT': {
-//             return {
-//                 ...state,
-//                 items: state.items.map(x => ({
-//                   ...x, 
-//                   title: x.id === payload.id ? payload.title : x.title  
-//                 }))
-//             };
-//         }
-//         //dispatch({type: 'REMOVE', payload: 1 })
-//         case 'REMOVE': {
-//             return {
-//                 ...state,
-//                 // 1, 2, 3 => 2, 3
-//                 items: state.items.filter(x => x.id !== payload)
-
-//             };
-//         }
-//         //dispatch({type: 'FINISH', payload: 1 })
-//         case 'FINISH': {
-//             return {
-//                 ...state,
-//                 items: state.items.map(x => ({
-//                   ...x, 
-//                   isFinish: x.id === payload ? true : x.isFinish  
-//                 }))
-                 
-//             };
-//         }
-//         //dispatch({type: 'REVERT', payload: 1 })
-//         case 'REVERT': {
-//             return {
-//                 ...state,
-//                 items: state.items.map(x => ({
-//                   ...x, 
-//                   isFinish: x.id === payload ? false : x.isFinish  
-//                 }))
-                 
-//             };
-//         }
-//         //dispatch({type: 'ADD', payload: { title: 'buy popuga food', id: 1, isDone: false}})
-//         case 'ADD': {
-//             return {
-//                 ...state,
-//                 items: [
-//                     {
-//                         // title: payload.title...
-//                         ...payload,
-//                     },
-//                     ...state.items
-//                 ]
-//             };
-//         }
-
-//         default: {
-//             return state;
-//         }
-//     }
-// }
-// const InitialTodoList = {
-//     //Список дел
-//     // { title: 'buy popuga food', id: 1, isDone: false}
-//     items: [] 
-
-// }
+import { store } from './store';
+import { AddInputContainer, ToDoListContainer } from '@containers';
 
 export const App = () => {
-    const [state, dispatch] = useReducer(ToDoListReducer, InitialTodoList);
-    return(
-        <ToDoContext.Provider value={{
-            state, dispatch
-        }}>
-            <div className={styles.App}>
-                <AddInputContainer />
-                <ToDoListContainer />
-            </div>
-        </ToDoContext.Provider>
-    )
+  return (
+    <Provider store={store}>
+      <div className={styles.App}>
+        <AddInputContainer />
+        <ToDoListContainer />
+      </div>
+    </Provider>
+  )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,11 @@
 export { ToDoListReducer, InitialTodoList } from './toDoReducer';
+export { store } from './store';
+export {
+  ToDoSliceReducer,
+  InitialToDo,
+  edit, 
+  remove, 
+  finish, 
+  revert, 
+  add
+} from './ToDOSlice';
