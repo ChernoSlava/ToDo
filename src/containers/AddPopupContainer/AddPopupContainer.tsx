61362904b47a29e505ca2644cef071588dbe31d1
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-03-09 23:36:54 +0300

    feat(backend): Change addPopupContainer
    meta: issue-13_backend

--- src/containers/AddPopupContainer/AddPopupContainer.tsx
+++ src/containers/AddPopupContainer/AddPopupContainer.tsx
@@ -5,6 +5,7 @@ import { getUniqueId } from '@utils';
 import { useDispatch, useSelector } from 'react-redux';
 import { AddPopup } from '../../components/AddPopup';
 import { getAddPopupContainerProps } from '../../store/selectors';
+import { ToDoItemType } from '@types';
 
 export const AddPopupContainer: React.FC = () => {
   const dispatch = useDispatch();
@@ -13,11 +14,25 @@ export const AddPopupContainer: React.FC = () => {
     isShow && (
       <AddPopup
         onApply={text => {
-          if (text) {
-            dispatch(add({ title: text, isFinish: false, id: getUniqueId() }));
+          if (!text) {
+            return
           }
-          dispatch(closePopup('add'));
-          dispatch(setCurrent(null));
+          fetch('/api/v1/todo', {
+            method: 'POST',
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              title: text,
+            })
+          })
+            .then(res => res.json())
+            .then(json => {
+              dispatch(add({ title: text, isFinish: false, id: (json as ToDoItemType).id }));
+              dispatch(closePopup('add'));
+              dispatch(setCurrent(null));
+          })
+          
         }}
         onCancel={() => {
           dispatch(closePopup('add'));
